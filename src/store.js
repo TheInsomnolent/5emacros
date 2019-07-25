54bdb77acfa832793849105dc6ebb801eb473d57
Matthew Griffiths <griff@me3d.com>
2019-07-25 11:16:33 +1000

    Fixed sourcemaps

--- src/store.js
+++ src/store.js
@@ -3,7 +3,8 @@ import { createStore, applyMiddleware } from "redux"
 // Logger with default options
 import { createLogger } from "redux-logger"
 import reducer from "./reducer"
-
+let middlewares = []
+if (process.env.NODE_ENV === "production") middlewares.push(createLogger())
 export default function configureStore(initialState) {
-    return createStore(reducer, initialState, applyMiddleware(createLogger()))
+    return createStore(reducer, initialState, applyMiddleware(...middlewares))
 }
